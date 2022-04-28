592c394fb411887d22be47fcea6fcef3df643aa9
unknown <jaimesan1231@gmail.com>
2022-04-28 12:40:32 -0500

    Se creo el formato de la boleta

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { useEffect } from "react";
 import axios from "axios";
 import TicketPDF from "./TicketPDF";
 import { PDFViewer } from "@react-pdf/renderer";
+import { useState } from "react/cjs/react.development";
 
 function App() {
+  const [table,seTable]=useState([])
   useEffect(() => {
     axios
       .get("/Boleta.xml", {
@@ -38,16 +40,32 @@ function App() {
           });
 
         //  console.log(xml.getElementsByTagName("cbc:InvoicedQuantity").value)
+        const table=[]
+        xml.getElementsByTagName("cbc:Description").map((item,index)=>{
+          table.push({
+            descripcion:xml.getElementsByTagName("cbc:Description")[index].value,
+            cantidad:xml.getElementsByTagName("cbc:InvoicedQuantity")[index].value,
+            precio:prices[index],
+            subtotal:amounts[index],
+          })
+        })
+        seTable(table)
         console.log(amounts);
+        console.log(table);
+        console.log(xml.getElementsByTagName("cbc:InvoicedQuantity"));
+        console.log(xml.getElementsByTagName("cbc:Description"));
         console.log(xml.getElementsByTagName("cbc:TaxableAmount")[0].value);
         console.log(xml.getElementsByTagName("cbc:TaxAmount")[0].value);
         console.log(xml.getElementsByTagName("cbc:PayableAmount")[0].value);
+        console.log(xml.getElementsByTagName("cbc:Note")[0].value);
+        console.log(xml.getElementsByTagName("cbc:RegistrationName")[1].value);
+     
       });
   }, []);
   return (
     <div className="App">
       <PDFViewer style={{ width: "100%", height: "90vh" }}>
-        <TicketPDF />
+        <TicketPDF table={table}/>
       </PDFViewer>
     </div>
   );
