9898e918bcf00ccf58f663cf123e97b05bbfe3ab
jaimesan1231 <jaimesan1231@gmail.com>
2022-04-29 02:35:07 -0400

    se arreglo el formato de boleta

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { PDFViewer } from "@react-pdf/renderer";
 import { useState } from "react/cjs/react.development";
 
 function App() {
-  const [table,seTable]=useState([])
+  const [table, seTable] = useState([]);
+  const [boleta, setBoleta] = useState({});
   useEffect(() => {
     axios
       .get("/Boleta.xml", {
@@ -23,6 +24,26 @@ function App() {
         console.log(xml.getElementsByTagName("cbc:Line")[0].value);
         console.log(xml.getElementsByTagName("cbc:ID")[1].value);
         console.log(xml.getElementsByTagName("cbc:IssueDate")[0].value);
+        setBoleta({
+          id: xml.getElementsByTagName("cbc:ID")[1].value,
+          fecha: xml.getElementsByTagName("cbc:IssueDate")[0].value,
+          subtotal: xml.getElementsByTagName("cbc:TaxableAmount")[0].value,
+          igv: xml.getElementsByTagName("cbc:TaxAmount")[0].value,
+          nota: xml
+            .getElementsByTagName("cbc:Note")[0]
+            .value.substring(
+              0,
+              xml.getElementsByTagName("cbc:Note")[0].value.length - 1
+            ),
+          cliente: xml
+            .getElementsByTagName("cbc:RegistrationName")[1]
+            .value.substring(
+              0,
+              xml.getElementsByTagName("cbc:RegistrationName")[1].value.length -
+                1
+            ),
+          total: xml.getElementsByTagName("cbc:PayableAmount")[0].value,
+        });
         const prices = [];
         xml.getElementsByTagName("cbc:PriceAmount").map((item, index) => {
           if (index % 2 !== 0) {
@@ -40,16 +61,23 @@ function App() {
           });
 
         //  console.log(xml.getElementsByTagName("cbc:InvoicedQuantity").value)
-        const table=[]
-        xml.getElementsByTagName("cbc:Description").map((item,index)=>{
+        const table = [];
+        xml.getElementsByTagName("cbc:Description").map((item, index) => {
           table.push({
-            descripcion:xml.getElementsByTagName("cbc:Description")[index].value,
-            cantidad:xml.getElementsByTagName("cbc:InvoicedQuantity")[index].value,
-            precio:prices[index],
-            subtotal:amounts[index],
-          })
-        })
-        seTable(table)
+            descripcion: xml
+              .getElementsByTagName("cbc:Description")
+              [index].value.substring(
+                0,
+                xml.getElementsByTagName("cbc:Description")[index].value
+                  .length - 1
+              ),
+            cantidad: xml.getElementsByTagName("cbc:InvoicedQuantity")[index]
+              .value,
+            precio: prices[index],
+            subtotal: amounts[index],
+          });
+        });
+        seTable(table);
         console.log(amounts);
         console.log(table);
         console.log(xml.getElementsByTagName("cbc:InvoicedQuantity"));
@@ -59,13 +87,37 @@ function App() {
         console.log(xml.getElementsByTagName("cbc:PayableAmount")[0].value);
         console.log(xml.getElementsByTagName("cbc:Note")[0].value);
         console.log(xml.getElementsByTagName("cbc:RegistrationName")[1].value);
-     
+        console.log(
+          xml
+            .getElementsByTagName("cbc:Description")[1]
+            .value.substring(
+              0,
+              xml.getElementsByTagName("cbc:Description")[1].value.length - 1
+            )
+        );
+        console.log(
+          xml
+            .getElementsByTagName("cbc:RegistrationName")[1]
+            .value.substring(
+              0,
+              xml.getElementsByTagName("cbc:RegistrationName")[1].value.length -
+                1
+            )
+        );
+        console.log(
+          xml
+            .getElementsByTagName("cbc:Note")[0]
+            .value.substring(
+              0,
+              xml.getElementsByTagName("cbc:Note")[0].value.length - 1
+            )
+        );
       });
   }, []);
   return (
     <div className="App">
       <PDFViewer style={{ width: "100%", height: "90vh" }}>
-        <TicketPDF table={table}/>
+        <TicketPDF table={table} boleta={boleta} />
       </PDFViewer>
     </div>
   );
--- src/TicketPDF.js
+++ src/TicketPDF.js
@@ -1,85 +1,243 @@
 import React from "react";
-import { Document, Font, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
+import {
+  Document,
+  Font,
+  Page,
+  StyleSheet,
+  Text,
+  View,
+} from "@react-pdf/renderer";
 
-function TicketPDF({table}) {
-
+function TicketPDF({ table, boleta }) {
   Font.register({
     family: "Roboto Bold",
-    src:
-      "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf"
-  }
-  );
+    src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf",
+  });
   Font.register({
     family: "Roboto Light",
-    src:
-      "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf"
-  })
-  const styles=StyleSheet.create({
-    page:{
-      fontFamily:"Roboto Light",
-      maxWidth:"219px",
-      padding:"10px"
+    src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf",
+  });
+  const styles = StyleSheet.create({
+    page: {
+      fontFamily: "Roboto Light",
+      maxWidth: "219px",
+      padding: "10px",
     },
-    bolder:{
-      fontFamily:"Roboto Bold",
-    }
-  })
-  console.log(table)
+    bolder: {
+      fontFamily: "Roboto Bold",
+    },
+  });
+  console.log(table);
+  console.log(boleta);
   return (
-    <Document style={{width:"100px"}}>
-      <Page  style={styles.page}>
-        <View style={{textAlign:"center",maxWidth:"219px",margin:"0 auto 0 auto"}}>
-        <Text style={{fontSize:"10px",marginBottom:"10px",fontFamily:"Roboto Bold"}}>BODEGA BAZAR LIBRERIA JAIMITOS</Text>
-        <Text style={{fontSize:"10px"}}>RUC: 10218763320</Text>    
-        <Text style={{fontSize:"10px",marginTop:"10px"}}>AV. LOS DOMINICOS 555 URB. JORGE CHAVEZ IIETP ENTRE OLIVAR/TOMAS VALLE FTE COL.BAUTISTA</Text>
-        <Text style={{fontSize:"10px",marginTop:"10px",fontFamily:"Roboto Bold"}}>Boleta de Venta Electrónica</Text>
-        <Text style={{fontSize:"10px",marginBottom:"10px",fontFamily:"Roboto Bold"}}>E001-99</Text>
- 
-        <View>
-          <View style={{fontSize:"10px", width:"50%", display:"flex", flexDirection:"row",justifyContent:"space-between",marginBottom:"10px"}}>
-            <Text>Fecha:</Text>
-            <Text>2022-04-11</Text>
+    <Document style={{ width: "100px" }}>
+      <Page style={styles.page}>
+        <View
+          style={{
+            textAlign: "center",
+            maxWidth: "219px",
+            margin: "0 auto 0 auto",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: "10px",
+              marginBottom: "10px",
+              fontFamily: "Roboto Bold",
+            }}
+          >
+            BODEGA BAZAR LIBRERIA JAIMITOS
+          </Text>
+          <Text style={{ fontSize: "10px" }}>RUC: 10218763320</Text>
+          <Text style={{ fontSize: "10px", marginTop: "10px" }}>
+            AV. LOS DOMINICOS 555 URB. JORGE CHAVEZ IIETP ENTRE OLIVAR/TOMAS
+            VALLE FTE COL.BAUTISTA
+          </Text>
+          <Text
+            style={{
+              fontSize: "10px",
+              marginTop: "10px",
+              fontFamily: "Roboto Bold",
+            }}
+          >
+            Boleta de Venta Electrónica
+          </Text>
+          <Text
+            style={{
+              fontSize: "10px",
+              marginBottom: "10px",
+              fontFamily: "Roboto Bold",
+            }}
+          >
+            {boleta.id}
+          </Text>
+
+          <View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "50%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: "10px",
+              }}
+            >
+              <Text>Fecha:</Text>
+              <Text>2022-04-11</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginBottom: "10px",
+              }}
+            >
+              <Text>Cliente:</Text>
+              <Text style={styles.boldga}>{boleta.cliente}</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "100%",
+                flexDirection: "row",
+                marginBottom: "10px",
+              }}
+            >
+              <Text
+                style={{
+                  width: "46%",
+                  textAlign: "left",
+                  fontFamily: "Roboto Bold",
+                }}
+              >
+                Descripcion
+              </Text>
+              <Text
+                style={{
+                  width: "18%",
+                  textAlign: "left",
+                  fontFamily: "Roboto Bold",
+                }}
+              >
+                Cant
+              </Text>
+              <Text
+                style={{
+                  width: "18%",
+                  textAlign: "left",
+                  fontFamily: "Roboto Bold",
+                }}
+              >
+                Precio
+              </Text>
+              <Text
+                style={{
+                  width: "18%",
+                  textAlign: "left",
+                  fontFamily: "Roboto Bold",
+                }}
+              >
+                SubT
+              </Text>
+            </View>
+            {table.map((item, index) => (
+              <View
+                style={{
+                  fontSize: "10px",
+                  width: "100%",
+                  flexDirection: "row",
+                  marginBottom: "5px",
+                }}
+              >
+                <Text style={{ width: "46%", textAlign: "left" }}>
+                  {item.descripcion}
+                </Text>
+                <Text style={{ width: "18%", textAlign: "left" }}>
+                  {item.cantidad}
+                </Text>
+                <Text style={{ width: "18%", textAlign: "left" }}>
+                  {item.precio}
+                </Text>
+                <Text style={{ width: "18%", textAlign: "left" }}>
+                  {item.subtotal}
+                </Text>
+              </View>
+            ))}
+            <View
+              style={{
+                fontSize: "10px",
+                width: "50%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginLeft: "50%",
+                marginTop: "10px",
+              }}
+            >
+              <Text>Subtotal:</Text>
+              <Text>{boleta.subtotal}</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "50%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginLeft: "50%",
+                marginTop: "10px",
+              }}
+            >
+              <Text>IGV:</Text>
+              <Text>{boleta.igv}</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "50%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginLeft: "50%",
+                marginTop: "10px",
+              }}
+            >
+              <Text>Total:</Text>
+              <Text>{boleta.total}</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "10px",
+                width: "100%",
+                textAlign: "center",
+                marginTop: "10px",
+              }}
+            >
+              <Text>{boleta.nota}</Text>
+            </View>
+            <View
+              style={{
+                fontSize: "7px",
+                width: "100%",
+                textAlign: "center",
+                marginTop: "10px",
+              }}
+            >
+              <Text>
+                {" "}
+                Esta es una representación impresa de la Boleta de Venta
+                Electrónica, generada en el Sistema de la SUNAT. El Emisor
+                Electrónico puede verificarla utilizando su clave SOL, el
+                Adquirente o Usuario puede consultar su validez en SUNAT
+                Virtual: www.sunat.gob.pe, en Opciones sin Clave SOL/ Consulta
+                de Validez del CPE.{" "}
+              </Text>
+            </View>
           </View>
-          <View style={{fontSize:"10px", width:"100%", display:"flex", flexDirection:"row",justifyContent:"space-around", marginBottom:"10px"}}>
-            <Text>Cliente:</Text>
-            <Text style={styles.boldga}>CARMEN ROSA SANCHEZ LOPEZ </Text>
-          </View>
-          <View style={{fontSize:"10px", width:"100%", flexDirection:"row", marginBottom:"10px"}}>
-            <Text style={{width:"46%", textAlign:"left",fontFamily:"Roboto Bold"}}>Descripcion</Text>
-            <Text style={{width:"18%", textAlign:"left",fontFamily:"Roboto Bold"}}>Cant</Text>
-            <Text style={{width:"18%", textAlign:"left",fontFamily:"Roboto Bold"}}>Precio</Text>
-            <Text style={{width:"18%", textAlign:"left",fontFamily:"Roboto Bold"}}>SubT</Text>
-          </View>
-          {
-            table.map((item,index)=>(
-              <View style={{fontSize:"10px", width:"100%", flexDirection:"row",marginBottom:"5px"}}>
-            <Text style={{width:"46%", textAlign:"left"}}>{item.descripcion}</Text>
-            <Text style={{width:"18%", textAlign:"left"}}>{item.cantidad}</Text>
-            <Text style={{width:"18%", textAlign:"left"}}>{item.precio}</Text>
-            <Text style={{width:"18%", textAlign:"left"}}>{item.subtotal}</Text>
-          </View>
-            ))
-          }
-          <View style={{fontSize:"10px", width:"50%", display:"flex", flexDirection:"row",justifyContent:"space-around", marginLeft:"50%",marginTop:"10px"}}>
-            <Text>Subtotal:</Text>
-            <Text>24.15</Text>
-          </View>
-          <View style={{fontSize:"10px", width:"50%", display:"flex", flexDirection:"row",justifyContent:"space-around", marginLeft:"50%",marginTop:"10px"}}>
-            <Text>IGV:</Text>
-            <Text>4.35</Text>
-          </View>
-          <View style={{fontSize:"10px", width:"50%", display:"flex", flexDirection:"row",justifyContent:"space-around", marginLeft:"50%",marginTop:"10px"}}>
-            <Text>Total:</Text>
-            <Text>28.50</Text>
-          </View>
-          <View style={{fontSize:"10px", width:"100%",textAlign:"center",marginTop:"10px"}}>
-            <Text>SON:  VEINTIOCHO Y 50/100  SOLES </Text>
-          </View>
-          <View style={{fontSize:"7px", width:"100%",textAlign:"center",marginTop:"10px"}}>
-            <Text> Esta es una representación impresa de la Boleta de Venta Electrónica, generada en el Sistema de la SUNAT. El Emisor Electrónico puede verificarla utilizando su clave SOL, el Adquirente o Usuario puede consultar su validez en SUNAT Virtual: www.sunat.gob.pe, en Opciones sin Clave SOL/ Consulta de Validez del CPE. </Text>
-          </View>
-         
-        </View>
         </View>
       </Page>
     </Document>
